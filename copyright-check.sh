397b41099f6dbe92b60c668ab5d916be1c40a249
Chunxi Luo <Chunxi.Luo@ibm.com>
2019-08-02 10:28:27 -0400

    Grc-ui copyright-check year-range is hardcoded  (#134)

--- copyright-check.sh
+++ copyright-check.sh
@@ -1,33 +1,44 @@
-#!/bin/bash
-
 # Licensed Materials - Property of IBM
-# (c) Copyright IBM Corporation 2018. All Rights Reserved.
+# (c) Copyright IBM Corporation 2018, 2019. All Rights Reserved.
 # Note to U.S. Government Users Restricted Rights:
 # Use, duplication or disclosure restricted by GSA ADP Schedule
 # Contract with IBM Corp.
 
-YEAR=2019
+#!/bin/bash
 
-#LINE1="${COMMENT_PREFIX}Licensed Materials - Property of IBM"
-CHECK1=" Licensed Materials - Property of IBM"
-#LINE2="${COMMENT_PREFIX}(c) Copyright IBM Corporation ${YEAR}. All Rights Reserved."
-CHECK2=" Copyright IBM Corporation 2018. All Rights Reserved."
-CHECK2a=" Copyright IBM Corporation 2017. All Rights Reserved."
-CHECK2b=" Copyright IBM Corporation 2016, 2018. All Rights Reserved."
-CHECK2c=" Copyright IBM Corporation 2017, 2018. All Rights Reserved."
-CHECK2d=" Copyright IBM Corporation 2019. All Rights Reserved."
-CHECK2e=" Copyright IBM Corporation 2017, 2019. All Rights Reserved."
-CHECK2f=" Copyright IBM Corporation 2018, 2019. All Rights Reserved."
-#LINE3="${COMMENT_PREFIX}Note to U.S. Government Users Restricted Rights:"
-CHECK3=" Note to U.S. Government Users Restricted Rights:"
-#LINE4="${COMMENT_PREFIX}Use, duplication or disclosure restricted by GSA ADP Schedule"
-CHECK4=" Use, duplication or disclosure restricted by GSA ADP Schedule"
-#LINE5="${COMMENT_PREFIX}Contract with IBM Corp."
-CHECK5=" Contract with IBM Corp."
+#Project start year
+origin_year=2016
+#Back up year if system time is null or incorrect
+back_up_year=2019
+#Currrent year
+current_year=$(date +"%Y")
 
-#LIC_ARY to scan for
-LIC_ARY=("$CHECK1" "$CHECK2" "$CHECK3" "$CHECK4" "$CHECK5")
-LIC_ARY_SIZE=${#LIC_ARY[@]}
+if [ -z "$current_year" ] || [ $current_year -lt $origin_year ]; then
+  echo "Can't get correct system time\n  >>Use back_up_year=$back_up_year as current_year to check copyright in the file $f\n"
+  current_year=$back_up_year
+fi
+
+#lic_year to scan for year formart single line's correctness
+lic_year=()
+#All possible combination within [origin_year, current_year] range is valid format
+#seq isn't recommanded after bash version 3.0
+for ((start_year=origin_year;start_year<=current_year;start_year++)); 
+do
+  lic_year+=(" (c) Copyright IBM Corporation ${start_year}. All Rights Reserved.")
+  for ((end_year=start_year+1;end_year<=current_year;end_year++)); 
+  do
+    lic_year+=(" (c) Copyright IBM Corporation ${start_year}, ${end_year}. All Rights Reserved.")
+  done
+done
+lic_year_size=${#lic_year[@]}
+
+#lic_rest to scan for rest copyright format's correctness
+lic_rest=()
+lic_rest+=(" Licensed Materials - Property of IBM")
+lic_rest+=(" Note to U.S. Government Users Restricted Rights:")
+lic_rest+=(" Use, duplication or disclosure restricted by GSA ADP Schedule")
+lic_rest+=(" Contract with IBM Corp.")
+lic_rest_size=${#lic_rest[@]}
 
 #Used to signal an exit
 ERROR=0
@@ -35,7 +46,7 @@ ERROR=0
 
 echo "##### Copyright check #####"
 #Loop through all files. Ignore .FILENAME types
-for f in `find . -type f ! -iname ".*" ! -path "./build-harness/*" ! -path "./public/*" ! -path "./sslcert/*" ! -path "./node_modules/*" ! -path "./coverage/*"`; do
+for f in `find . -type f ! -path "./.eslintrc.js" ! -path "./build-harness/*" ! -path "./sslcert/*" ! -path "./node_modules/*" ! -path "./coverage/*" ! -path "./test-output/*" ! -path "./build/*" ! -path "./nls/*" ! -path "./public/*"`; do
   if [ ! -f "$f" ] || [ "$f" = "./copyright-check.sh" ]; then
     continue
   fi
@@ -50,35 +61,41 @@ for f in `find . -type f ! -iname ".*" ! -path "./build-harness/*" ! -path "./pu
   esac
 
   #Read the first 10 lines, most Copyright headers use the first 6 lines.
-  HEADER=`head -10 $f`
+  header=`head -10 $f`
   printf " Scanning $f . . . "
 
-  #Check for all copyright lines
-  for i in `seq 0 $((${LIC_ARY_SIZE}+1))`; do
-    #Add a status message of OK, if all copyright lines are found
-    if [ $i -eq ${LIC_ARY_SIZE} ]; then
-      printf "OK\n"
-    else
-      #Validate the copyright line being checked is present
-      if [[ $i == 1
-        && "$HEADER" != *"${CHECK2}"*
-        && "$HEADER" != *"${CHECK2a}"*
-        && "$HEADER" != *"${CHECK2b}"*
-        && "$HEADER" != *"${CHECK2c}"* 
-        && "$HEADER" != *"${CHECK2d}"* 
-        && "$HEADER" != *"${CHECK2e}"* 
-        && "$HEADER" != *"${CHECK2f}"* ]]; then
-        printf "Missing copyright\n  >>Could not find [${LIC_ARY[$i]}] in the file $f\n"
-        ERROR=1
-        break
-      fi
-      if [[ "$HEADER" != *"${LIC_ARY[$i]}"* && $i != 1 ]]; then
-        printf "Missing copyright\n  >>Could not find [${LIC_ARY[$i]}] in the file $f\n"
-        ERROR=1
-        break
-      fi
+  #Check for year copyright single line
+  year_line_count=0
+  for ((i=0;i<${lic_year_size};i++));
+  do
+    #Validate year formart within [origin_year, current_year] range
+    if [[ "$header" == *"${lic_year[$i]}"* ]]; then
+      year_line_count=$((year_line_count + 1))
     fi
   done
+
+  #Must find and only find one line valid year, otherwise invalid copyright formart
+  if [[ $year_line_count != 1 ]]; then
+    printf "Missing copyright\n  >>Could not find correct copyright year in the file $f\n"
+    ERROR=1
+    break
+  fi
+
+  #Check for rest copyright lines
+  for ((i=0;i<${lic_rest_size};i++));
+  do
+    #Validate the copyright line being checked is present
+    if [[ "$header" != *"${lic_rest[$i]}"* ]]; then
+      printf "Missing copyright\n  >>Could not find [${lic_rest[$i]}] in the file $f\n"
+      ERROR=1
+      break
+    fi
+  done
+
+  #Add a status message of OK, if all copyright lines are found
+  if [[ "$ERROR" == 0 ]]; then
+    printf "OK\n"
+  fi
 done
 
 echo "##### Copyright check ##### ReturnCode: ${ERROR}"
