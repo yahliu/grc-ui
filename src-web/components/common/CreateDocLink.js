5719f7e41d41f3244e3cafd767569bef9654beca
Xuning Kang <60612794+akang0@users.noreply.github.com>
2020-03-03 22:40:59 +0800

    Remove doc link (#20)

--- src-web/components/common/CreateDocLink.js
+++ src-web/components/common/CreateDocLink.js
@@ -10,22 +10,23 @@
 
 import React from 'react'
 import { Button } from 'carbon-components-react'
-import msgs from '../../../nls/platform.properties'
-import config from '../../../lib/shared/config'
+// import msgs from '../../../nls/platform.properties'
+// import config from '../../../lib/shared/config'
 
 const createDocLink = (locale, handleCreateResource, submitBtnTextKey, createFlag=true) => {
-  const vNumber = config['ICP_VERSION']
+  // const vNumber = config['ICP_VERSION']
 
   return (
     <div className={'button-group'}>
       {createFlag && <Button icon="add--glyph" small id='create-resource' onClick={handleCreateResource}>
         { submitBtnTextKey }
       </Button>}
-      <div className={'buttons'}>
+      { //Remove doc link temporarily, to be reverted.
+      /* <div className={'buttons'}>
         <button className={'bx--btn bx--btn--sm bx--btn--secondary'}>
           <a href={`https://www${config['env']==='development' ? '-03preprod': ''}.ibm.com/support/knowledgecenter/SSBS6K_${vNumber}/mcm/compliance/compliance_intro.html`} className={'doc-link'} target='doc' aria-describedby='launchWindow'>{msgs.get('button.view.doc', locale)}</a>
         </button>
-      </div>
+      </div> */}
     </div>
   )
 }
