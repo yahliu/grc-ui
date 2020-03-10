1dbbfa510a96478266949582aff7aa12615d5c9c
Yu Cao <ycao@redhat.com>
2020-03-10 09:22:59 -0400

    Update policies templates for Q1 release (#27)

--- src-web/components/templates/index.js
+++ src-web/components/templates/index.js
@@ -10,34 +10,34 @@
 
 import specPodRole from './spec-rolebinding.yaml'
 import specPodSecurity from './spec-psp.yaml'
-import specPodNetwork from './spec-network.yaml'
+// import specPodNetwork from './spec-network.yaml'
 import specPodLimit from './spec-mem-limit.yaml'
 import specPodExists from './spec-pod.yaml'
 import specRoles from './spec-roles.yaml'
 import specNamespace from './spec-namespace.yaml'
 import specClustadminrole from './spec-clusteradminrole.yaml'
 import specCertmgmtexp from './spec-certmgmtexp.yaml'
-import specMutationAdvisor from './spec-mutationadvisor.yaml'
-import specVAsecurity from './spec-vasecurity.yaml'
-import specKubeEncryption from './spec-kubeencryption.yaml'
-import specAudit from './spec-audit.yaml'
+// import specMutationAdvisor from './spec-mutationadvisor.yaml'
+// import specVAsecurity from './spec-vasecurity.yaml'
+// import specKubeEncryption from './spec-kubeencryption.yaml'
+// import specAudit from './spec-audit.yaml'
 import specCis from './spec-cis.yaml'
 import specCisOcp from './spec-cis-ocp.yaml'
 
 export {
   specPodRole,
   specPodSecurity,
-  specPodNetwork,
+  // specPodNetwork,
   specPodLimit,
   specPodExists,
   specRoles,
   specNamespace,
   specClustadminrole,
   specCertmgmtexp,
-  specMutationAdvisor,
-  specVAsecurity,
-  specKubeEncryption,
-  specAudit,
+  // specMutationAdvisor,
+  // specVAsecurity,
+  // specKubeEncryption,
+  // specAudit,
   specCis,
   specCisOcp,
 }
