aeccc7d0e9467fd988812018ef56e5037722e019
Yu Cao <ycao@redhat.com>
2020-05-29 17:27:41 -0400

    reenable e2e test cases (#104)

--- tests/e2e/config-policy-controller.test.js
+++ tests/e2e/config-policy-controller.test.js
@@ -16,7 +16,7 @@ const config = require('../../config')
 let page
 
 module.exports = {
-  '@disabled': true,
+  '@disabled': false,
 
   before: (browser) => {
     const loginPage = browser.page.LoginPage()
@@ -24,45 +24,47 @@ module.exports = {
     loginPage.authenticate()
 
     const url = `${browser.launch_url}${config.get('contextPath')}/all`
-    page = browser.page.PolicyController()
+    page = browser.page.ConfigPolicyController()
     page.navigate(url)
   },
 
   'Policy controller: single object / musthave + mustnothave': (browser) => {
     const time = browser.globals.time
-    const singleMustHaveInform = fs.readFileSync(path.join(__dirname, 'yaml/single_musthave_inform.yaml'))
+    const singleMustHaveInform = fs.readFileSync(path.join(__dirname, 'yaml/config_policy/single_musthave_inform.yaml'))
     let yaml = singleMustHaveInform.toString()
     page.createPolicy(browser, 'policy-pod-single-musthave-inform-' + time, yaml, time)
-    page.checkViolations('policy-pod-single-musthave-inform-' + time, true, 'pods `nginx-pod-' + time + '` is missing, and should be created')
+    page.checkViolations('policy-pod-single-musthave-inform-' + time, true, 'NonCompliant; violation - pods `nginx-pod-' + time + '` does not exist as specified, and should be created')
     page.deletePolicy('policy-pod-single-musthave-inform-' + time)
     browser.pause(1000)
-    const singleMustHaveEnforce = fs.readFileSync(path.join(__dirname, 'yaml/single_musthave_enforce.yaml'))
+    const singleMustHaveEnforce = fs.readFileSync(path.join(__dirname, 'yaml/config_policy/single_musthave_enforce.yaml'))
     yaml = singleMustHaveEnforce.toString()
     page.createPolicy(browser, 'policy-pod-single-musthave-enforce-' + time, yaml, time)
+    browser.pause(20000) // wait for policy to create pod
     page.checkViolations('policy-pod-single-musthave-enforce-' + time, false)
     page.deletePolicy('policy-pod-single-musthave-enforce-' + time)
-    const singleMustNotHaveInform = fs.readFileSync(path.join(__dirname, 'yaml/single_mustnothave_inform.yaml'))
+    const singleMustNotHaveInform = fs.readFileSync(path.join(__dirname, 'yaml/config_policy/single_mustnothave_inform.yaml'))
     yaml = singleMustNotHaveInform.toString()
     page.createPolicy(browser, 'policy-pod-single-mustnothave-inform-' + time, yaml, time)
-    page.checkViolations('policy-pod-single-mustnothave-inform-' + time, true, 'pods exist and should be deleted: [nginx-pod-' + time + '] in namespace default')
+    page.checkViolations('policy-pod-single-mustnothave-inform-' + time, true, 'NonCompliant; violation - pods exist and should be deleted: [nginx-pod-' + time + '] in namespace default')
     page.deletePolicy('policy-pod-single-mustnothave-inform-' + time)
     browser.pause(1000)
-    const singleMustNotHaveEnforce = fs.readFileSync(path.join(__dirname, 'yaml/single_mustnothave_enforce.yaml'))
+    const singleMustNotHaveEnforce = fs.readFileSync(path.join(__dirname, 'yaml/config_policy/single_mustnothave_enforce.yaml'))
     yaml = singleMustNotHaveEnforce.toString()
     page.createPolicy(browser, 'policy-pod-single-mustnothave-enforce-' + time, yaml, time)
+    browser.pause(20000) // wait for policy to delete pod
     page.checkViolations('policy-pod-single-mustnothave-enforce-' + time, false)
     page.deletePolicy('policy-pod-single-mustnothave-enforce-' + time)
   },
 
   'Policy controller: all objects of kind / exists': (browser) => {
     const time = browser.globals.time
-    const kindMustNotHaveNC = fs.readFileSync(path.join(__dirname, 'yaml/kind_mustnothave_noncompliant.yaml'))
+    const kindMustNotHaveNC = fs.readFileSync(path.join(__dirname, 'yaml/config_policy/kind_mustnothave_noncompliant.yaml'))
     let yaml = kindMustNotHaveNC.toString()
     page.createPolicy(browser, 'policy-ns-mustnothave-' + time, yaml, time)
     page.checkViolations('policy-ns-mustnothave-' + time, true)
     page.deletePolicy('policy-ns-mustnothave-' + time)
     browser.pause(1000)
-    const kindMustHave = fs.readFileSync(path.join(__dirname, 'yaml/kind_musthave_compliant.yaml'))
+    const kindMustHave = fs.readFileSync(path.join(__dirname, 'yaml/config_policy/kind_musthave_compliant.yaml'))
     yaml = kindMustHave.toString()
     page.createPolicy(browser, 'policy-ns-musthave-' + time, yaml, time)
     page.checkViolations('policy-ns-musthave-' + time, false)
@@ -71,31 +73,30 @@ module.exports = {
 
   'Policy controller: all objects of kind / does not exist': (browser) => {
     const time = browser.globals.time
-    const createNS = fs.readFileSync(path.join(__dirname, 'yaml/create_test_ns.yaml'))
+    const createNS = fs.readFileSync(path.join(__dirname, 'yaml/config_policy/create_test_ns.yaml'))
     let yaml = createNS.toString()
     page.createPolicy(browser, 'policy-namespace-create-' + time, yaml, time)
-    browser.pause(30000) //Wait until policy acted
+    browser.pause(20000) //Wait until policy acted
     page.deletePolicy('policy-namespace-create-' + time)
     browser.pause(1000)
     //do checks
-    const kindMustHave = fs.readFileSync(path.join(__dirname, 'yaml/kind_musthave_noncompliant.yaml'))
+    const kindMustHave = fs.readFileSync(path.join(__dirname, 'yaml/config_policy/kind_musthave_noncompliant.yaml'))
     yaml = kindMustHave.toString()
     page.createPolicy(browser, 'policy-pod-musthave-all-' + time, yaml, time)
-    browser.pause(120000) //Wait until policy acted
-    page.checkViolations('policy-pod-musthave-all-' + time, true, 'No instances of `pods` exist as specified, and one should be created')
+    page.checkViolations('policy-pod-musthave-all-' + time, true, 'NonCompliant; violation - No instances of `pods` exist as specified, and one should be created')
     page.deletePolicy('policy-pod-musthave-all-' + time)
     browser.pause(1000)
-    const kindMustNotHave = fs.readFileSync(path.join(__dirname, 'yaml/kind_mustnothave_compliant.yaml'))
+    const kindMustNotHave = fs.readFileSync(path.join(__dirname, 'yaml/config_policy/kind_mustnothave_compliant.yaml'))
     yaml = kindMustNotHave.toString()
     page.createPolicy(browser, 'policy-pod-mustnothave-all-' + time, yaml, time)
-    browser.pause(30000) //Wait until policy acted
+    browser.pause(20000) //Wait until policy acted
     page.checkViolations('policy-pod-mustnothave-all-' + time, false)
     page.deletePolicy('policy-pod-mustnothave-all-' + time)
     //delete ns
-    const deleteNS = fs.readFileSync(path.join(__dirname, 'yaml/delete_test_ns.yaml'))
+    const deleteNS = fs.readFileSync(path.join(__dirname, 'yaml/config_policy/delete_test_ns.yaml'))
     yaml = deleteNS.toString()
     page.createPolicy(browser, 'policy-namespace-delete-' + time, yaml, time)
-    browser.pause(30000) //Wait until policy acted
+    browser.pause(20000) //Wait until policy acted
     page.deletePolicy('policy-namespace-delete-' + time)
     browser.pause(1000)
   },
--- tests/page-objects/ConfigPolicyController.js
+++ tests/page-objects/ConfigPolicyController.js
@@ -77,7 +77,6 @@ function createPolicy(browser, name, yaml, time) {
   this.expect.element('.section-title:nth-of-type(1)').text.to.equal('Policy details')
   this.expect.element('.new-structured-list > table:nth-child(1) > tbody > tr:nth-child(1) > td:nth-child(2)').text.to.equal(name)
   this.expect.element('.overview-content > div:nth-child(2) > .section-title').text.to.equal('Placement')
-  browser.pause(35000)
   this.waitForElementVisible('.overview-content-second > div:nth-child(1) > div > div > div:nth-child(1) > .bx--module__title')
   this.expect.element('.overview-content-second > div:nth-child(1) > div > div > div:nth-child(1) > .bx--module__title').text.to.equal('Placement rule')
   this.expect.element('.overview-content-second > div:nth-child(1) > div > div > .bx--module__content > section > div > div:nth-child(1) > div:nth-child(2)').text.to.equal('placement-' + name)
