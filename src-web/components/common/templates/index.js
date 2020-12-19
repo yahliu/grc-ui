22508d23ca5d950e150cbdb9a55e1d33433aedc8
Dale Haiducek <dhaiduce@redhat.com>
2020-12-19 12:53:19 -0500

    Template fixes - 8081 & 8088 (#365)

--- src-web/components/common/templates/index.js
+++ src-web/components/common/templates/index.js
@@ -9,34 +9,34 @@
 /* Copyright (c) 2020 Red Hat, Inc. */
 'use strict'
 
-import specPodRole from './spec-rolebinding.yaml'
-import specPodSecurity from './spec-psp.yaml'
-import specPodLimit from './spec-mem-limit.yaml'
-import specPodExists from './spec-pod.yaml'
-import specRoles from './spec-roles.yaml'
-import specNamespace from './spec-namespace.yaml'
-import specClustadminrole from './spec-clusteradminrole.yaml'
 import specCertmgmtexp from './spec-certmgmtexp.yaml'
-import specScc from './spec-scc.yaml'
-import specImv from './spec-imagemanifestvuln.yaml'
+import specClusteradminrole from './spec-clusteradminrole.yaml'
+import specCompOperator from './spec-comp-operator.yaml'
+import specContainerMemLimit from './spec-container-mem-limit.yaml'
 import specEtcdEncryption from './spec-etcd-encryption.yaml'
 import specGatekeeper from './spec-gatekeeper.yaml'
-import specCompOperator from './spec-comp-operator.yaml'
+import specImagemanifestvuln from './spec-imagemanifestvuln.yaml'
+import specNamespace from './spec-namespace.yaml'
+import specPodExists from './spec-pod-exists.yaml'
+import specPodSecurity from './spec-pod-security.yaml'
+import specRolebinding from './spec-rolebinding.yaml'
+import specRoles from './spec-roles.yaml'
+import specScc from './spec-scc.yaml'
 
 const Choices = {
-  specPodRole,
-  specPodSecurity,
-  specPodLimit,
-  specPodExists,
-  specRoles,
-  specNamespace,
-  specClustadminrole,
   specCertmgmtexp,
-  specScc,
-  specImv,
+  specClusteradminrole,
+  specCompOperator,
+  specContainerMemLimit,
   specEtcdEncryption,
   specGatekeeper,
-  specCompOperator,
+  specImagemanifestvuln,
+  specNamespace,
+  specPodExists,
+  specPodSecurity,
+  specRolebinding,
+  specRoles,
+  specScc,
 }
 
 export default Choices
