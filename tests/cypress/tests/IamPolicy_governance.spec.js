1de6a0c4e007a8ac05798c781326e49d7450787c
Karel Srot <kkaarreell@users.noreply.github.com>
2021-01-13 18:01:30 +0100

    added setup and cleanup to IamPolicy_governance.spec.js (#387)

--- tests/cypress/tests/IamPolicy_governance.spec.js
+++ tests/cypress/tests/IamPolicy_governance.spec.js
@@ -1,7 +1,16 @@
 /* Copyright (c) 2020 Red Hat, Inc. */
 /// <reference types="cypress" />
 import { test_genericPolicyGovernance } from './common/generic_policies_governance'
+import { cleanup_usingPolicyYAML } from './common/generic_policy_cleanup'
+
+describe('IamPolicy governance - setup', () => {
+  cleanup_usingPolicyYAML('IamPolicy_governance/policy-clusterrolebinding-setup_raw.yaml')
+})
 
 describe('IamPolicy governance', () => {
   test_genericPolicyGovernance('IamPolicy_governance/policy-config.yaml', 'IamPolicy_governance/violations-inform.yaml', 'IamPolicy_governance/violations-inform.yaml')
+})
+
+describe('IamPolicy governance - cleanup', () => {
+  cleanup_usingPolicyYAML('IamPolicy_governance/policy-clusterrolebinding-cleanup_raw.yaml')
 })
