b9f2a177798f9ac17032ddaaa97e31cfb69d6216
Alex Luo <55596988+ChunxiAlexLuo@users.noreply.github.com>
2021-01-29 15:27:18 -0500

    Update view yaml call to use new search api (#441)

--- src-web/tableDefinitions/relatedObjectsDef.js
+++ src-web/tableDefinitions/relatedObjectsDef.js
@@ -61,7 +61,15 @@ function buildViewYamlLink(item, locale) {
     ? _.get(item, 'object.metadata.selfLink')
     : _.get(item, 'selfLink')
   const cluster = _.get(item, 'cluster')
-  if (selfLink && cluster) {
+  const name = _.get(item, 'object.metadata.name')
+  const namespace = _.get(item, 'object.metadata.namespace')
+  const apiVersion = _.get(item, 'object.apiVersion')
+  const kind = _.get(item, 'object.kind')
+  if (cluster && kind && apiVersion && name) {
+    return <a target='_blank' rel='noopener noreferrer'
+    href={`/resources?cluster=${cluster}&kind=${kind}&apiversion=${apiVersion}&namespace=${namespace}&name=${name}`}>{msgs.get('table.actions.view.yaml', locale)}</a>
+  }
+  else if (selfLink && cluster) {
     return <a target='_blank' rel='noopener noreferrer'
       href={`/multicloud/details/${cluster}${selfLink}`}>{msgs.get('table.actions.view.yaml', locale)}</a>
   }
