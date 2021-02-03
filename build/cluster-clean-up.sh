279aee2efd7a05c67e6543078fc61cb344fba921
Yu Cao <ycao@redhat.com>
2021-02-03 11:56:49 -0500

    address warning msg during the clean up steps (#446)

--- build/cluster-clean-up.sh
+++ build/cluster-clean-up.sh
@@ -23,11 +23,11 @@ function managed() {
     oc delete ClusterServiceVersion -n openshift-operators container-security-operator.v3.3.4 || true
     oc delete crd imagemanifestvulns.secscan.quay.redhat.com || true
     oc delete LimitRange container-mem-limit-range -n default || true
-    oc delete ns prod -n default || true
-    oc delete psp restricted-psp -n default || true
+    oc delete ns prod || true
+    oc delete psp restricted-psp || true
     oc delete role deployments-role -n default || true
     oc delete rolebinding operatoruser-rolebinding -n default || true
-    oc delete scc restricted-scc -n default || true
+    oc delete scc restricted-scc || true
 }
 
 case $1 in
