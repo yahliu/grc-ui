5c78b937bbe1781c7b6cf16bfb524aab9640414b
anamdhar <anamdhar@redhat.com>
2021-02-09 17:42:38 +0530

    Added the copyright header

--- tests/cypress/tests/InvalidYaml_governance.spec.js
+++ tests/cypress/tests/InvalidYaml_governance.spec.js
@@ -1,3 +1,6 @@
+/* Copyright (c) 2020 Red Hat, Inc. */
+/// <reference types="cypress" />
+
 import { getConfigObject } from '../config'
 import { createPolicyFromYAML, getDefaultSubstitutionRules } from '../views/policy'
 
