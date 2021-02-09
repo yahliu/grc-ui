13e4e73c2e571025a61628fb25d099b6333b9477
anamdhar <anamdhar@redhat.com>
2021-02-09 23:00:41 +0530

    removed 'only'

--- tests/cypress/tests/InvalidYaml_governance.spec.js
+++ tests/cypress/tests/InvalidYaml_governance.spec.js
@@ -4,7 +4,7 @@ import { getConfigObject } from '../config'
 import { createPolicyFromYAML, getDefaultSubstitutionRules } from '../views/policy'
 
 describe('RHACM4K-247 - GRC UI: [P1][Sev1][policy-grc] Create policy with invalid yaml', () => {
-  it.only('Create policy should fail with invalid policy name in yaml', () => {
+  it('Create policy should fail with invalid policy name in yaml', () => {
     const confFilePolicy = 'InvalidYamlTests/InvalidPolicyName.yaml'
     const rawPolicyYAML = getConfigObject(confFilePolicy, 'raw', getDefaultSubstitutionRules())
     cy.visit('/multicloud/policies/create')
