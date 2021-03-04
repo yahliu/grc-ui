9025838ba509e3bc4abdb5561349cda6225e9c86
anamdhar <anamdhar@redhat.com>
2021-03-04 20:19:12 +0530

    Added delete policy iteration

--- tests/cypress/tests/DuplicatePolicyInDiffNS.spec.js
+++ tests/cypress/tests/DuplicatePolicyInDiffNS.spec.js
@@ -3,23 +3,36 @@
 import { getConfigObject } from '../config'
 import { getDefaultSubstitutionRules } from '../common/views'
 const substitutionRules = [ [/\[ID\]/g, Cypress.env('RESOURCE_ID')] ]
+const policyNames = []
 describe('RHACM4K-2342 - GRC UI: [P1][Sev1][policy-grc] Verify create policy with different NS', () => {
   it('Create Namespae policy to create template ns as duplicatetest', () => {
     const confFilePolicy = 'duplicatePolicyInDiffNS/create_ns_template.yaml'
     const rawPolicyYAML = getConfigObject(confFilePolicy, 'raw', getDefaultSubstitutionRules())
+    const policyName = rawPolicyYAML.replace(/\r?\n|\r/g, ' ').replace(/^.*?name:\s*/m, '').replace(/\s.*/m,'')
     cy.visit('/multicloud/policies/create')
       .createPolicyFromYAML(rawPolicyYAML)
+    policyNames.push(policyName)
   })
   it('Create Pod policy with default as namespace', () => {
     const confFilePolicy = 'duplicatePolicyInDiffNS/pod_template_original.yaml'
     const rawPolicyYAML = getConfigObject(confFilePolicy, 'raw', substitutionRules)
+    const policyName = rawPolicyYAML.replace(/\r?\n|\r/g, ' ').replace(/^.*?name:\s*/m, '').replace(/\s.*/m,'')
     cy.visit('/multicloud/policies/create')
       .createPolicyFromYAML(rawPolicyYAML)
+    policyNames.push(policyName)
   })
   it('Create Pod policy with duplicatetest as namespace', () => {
     const confFilePolicy = 'duplicatePolicyInDiffNS/pod_template_duplicate.yaml'
     const rawPolicyYAML = getConfigObject(confFilePolicy, 'raw', substitutionRules)
+    const policyName = rawPolicyYAML.replace(/\r?\n|\r/g, ' ').replace(/^.*?name:\s*/m, '').replace(/\s.*/m,'')
     cy.visit('/multicloud/policies/create')
       .createPolicyFromYAML(rawPolicyYAML)
+    policyNames.push(policyName)
+  })
+  it(`Delete created policies`, () => {
+    for (const policyName in policyNames) {
+       cy.actionPolicyActionInListing(policyName, 'Remove')
+       cy.verifyPolicyNotInListing(policyName)
+    }
   })
 })
