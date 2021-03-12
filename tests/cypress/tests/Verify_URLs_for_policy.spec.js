b02e5b8ce5cd506b4f3519429cbcedf74b2e25e5
anamdhar <anamdhar@redhat.com>
2021-03-12 11:26:21 +0530

    updated changes in spec

--- tests/cypress/tests/Verify_URLs_for_policy.spec.js
+++ tests/cypress/tests/Verify_URLs_for_policy.spec.js
@@ -5,46 +5,47 @@
 
 import { getConfigObject } from '../config'
 import { getDefaultSubstitutionRules } from '../common/views'
-const substitutionRules = getDefaultSubstitutionRules()
-var policyName = ''
+
 describe('RHACM4K-2354 - GRC UI: [P1][Sev1][policy-grc] Check existent and non-existent URLs for the policy', () => {
+  const substitutionRules = getDefaultSubstitutionRules()
+  let policyName = ''
   it('Create policy for the URL visit', () => {
     const confFilePolicy = 'duplicatePolicyInDiffNS/create_ns_template.yaml'
     const rawPolicyYAML = getConfigObject(confFilePolicy, 'raw', substitutionRules)
     policyName = rawPolicyYAML.replace(/\r?\n|\r/g, ' ').replace(/^.*?name:\s*/m, '').replace(/\s.*/m,'')
-    cy.visit('/multicloud/policies/create')
+    cy.visit('/multicloud/policies/create').waitForPageContentLoad()
       .createPolicyFromYAML(rawPolicyYAML)
   })
   it('Verify URLs for cluster name and policy that do exist', () => {
-    cy.visit('/multicloud/policies/policy/local-cluster/default.'+policyName)
+    cy.visit('/multicloud/policies/policy/local-cluster/default.'+policyName).waitForPageContentLoad()
     .checkPolicyNoResourcesIconMessage()
-    cy.visit('/multicloud/policies/policy/local-cluster/default.'+policyName+'/a/b/c')
+    cy.visit('/multicloud/policies/policy/local-cluster/default.'+policyName+'/a/b/c').waitForPageContentLoad()
     .checkPolicyNoResourcesIconMessage()
   })
   it('Verify URLs for cluster name and policy that dont exist', () => {
-    cy.visit('/multicloud/policies/policy/not-a-cluster/not-a-policy')
+    cy.visit('/multicloud/policies/policy/not-a-cluster/not-a-policy').waitForPageContentLoad()
     .checkPolicyNoResourcesIconMessage(false)
-    cy.visit('/multicloud/policies/policy/not-a-cluster/not-a-policy/a/b/c')
+    cy.visit('/multicloud/policies/policy/not-a-cluster/not-a-policy/a/b/c').waitForPageContentLoad()
     .checkPolicyNoResourcesIconMessage(false)
   })
   it('Verify URLs for Namespace and policy that do exist', () => {
-    cy.visit('/multicloud/policies/all/default/'+policyName)
+    cy.visit('/multicloud/policies/all/default/'+policyName).waitForPageContentLoad()
     .checkPolicyNoResourcesIconMessage()
-    cy.visit('/multicloud/policies/all/default/'+policyName+'/a/b/c')
+    cy.visit('/multicloud/policies/all/default/'+policyName+'/a/b/c').waitForPageContentLoad()
     .checkPolicyNoResourcesIconMessage()
-    cy.visit('/multicloud/policies/all/default/'+policyName+'/yaml')
+    cy.visit('/multicloud/policies/all/default/'+policyName+'/yaml').waitForPageContentLoad()
     .checkPolicyNoResourcesIconMessage()
-    cy.visit('/multicloud/policies/all/default/'+policyName+'/yaml/a/b/c')
+    cy.visit('/multicloud/policies/all/default/'+policyName+'/yaml/a/b/c').waitForPageContentLoad()
     .checkPolicyNoResourcesIconMessage()
   })
   it('Verify URLs for Namespace and policy that dont exist', () => {
-    cy.visit('/multicloud/policies/all/not-a-namespace/not-a-policy')
+    cy.visit('/multicloud/policies/all/not-a-namespace/not-a-policy').waitForPageContentLoad()
     .checkPolicyNoResourcesIconMessage(false)
-    cy.visit('/multicloud/policies/all/not-a-namespace/not-a-policy/status')
+    cy.visit('/multicloud/policies/all/not-a-namespace/not-a-policy/status').waitForPageContentLoad()
     .checkPolicyNoResourcesIconMessage(false, 'No policy status found')
-    cy.visit('/multicloud/policies/all/not-a-namespace/not-a-policy/yaml')
+    cy.visit('/multicloud/policies/all/not-a-namespace/not-a-policy/yaml').waitForPageContentLoad()
     .checkPolicyNoResourcesIconMessage(false, )
-    cy.visit('/multicloud/policies/all/not-a-namespace/not-a-policy/other/a/b/c')
+    cy.visit('/multicloud/policies/all/not-a-namespace/not-a-policy/other/a/b/c').waitForPageContentLoad()
     .checkPolicyNoResourcesIconMessage(false)
   })
 })
