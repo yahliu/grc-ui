d0196ba4ba2bdf790d57aaf17f3faf2a25def00a
Yu Cao <ycao@redhat.com>
2021-03-24 20:45:11 -0400

    only run nightwatch in extended phase in canary (#528)

--- tests/cypress/tests/Pod_governance.spec.js
+++ tests/cypress/tests/Pod_governance.spec.js
@@ -5,10 +5,10 @@
 import { describeT } from '../support/tagging'
 import { test_genericPolicyGovernance, test_applyPolicyYAML } from '../common/tests'
 
-describeT('RHACM4K-1724 - GRC UI: [P1][Sev1][policy-grc] Pod policy governance', () => {
+describeT('@extended RHACM4K-1724 - GRC UI: [P1][Sev1][policy-grc] Pod policy governance', () => {
     test_genericPolicyGovernance('Pod_governance/policy-config.yaml', 'Pod_governance/violations-inform.yaml', 'Pod_governance/violations-enforce.yaml')
 })
 
-describeT('GRC UI: [P1][Sev1][policy-grc] Pod policy governance - clean up', () => {
+describeT('@extended GRC UI: [P1][Sev1][policy-grc] Pod policy governance - clean up', () => {
     test_applyPolicyYAML('Pod_governance/Pod_specification_cleanup_policy_raw.yaml')
 })
