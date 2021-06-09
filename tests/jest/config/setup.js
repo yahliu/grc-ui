044055326c40d45aa8ab53afc580015cde243bd9
ChunxiAlexLuo <55596988+ChunxiAlexLuo@users.noreply.github.com>
2021-06-09 10:35:48 -0400

    Revert "Revert "save""
    This reverts commit e637a259fc574ad8c155025e3f9de89c7adaf937.
    
    Signed-off-by: ChunxiAlexLuo <55596988+ChunxiAlexLuo@users.noreply.github.com>

--- tests/jest/config/setup.js
+++ tests/jest/config/setup.js
@@ -13,7 +13,7 @@
 
 import React from 'react'
 import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 import 'cross-fetch/polyfill'
 import _uniqueId from 'lodash/uniqueId'
 import MockDate from 'mockdate'
